f17662910613b1ce2d229715da6aa52a18669f34
Jody Broad <msjodybroad@gmail.com>
2022-02-27 14:46:17 +0000

    added jody_rps_basic file and changes

--- jody_rps.py
+++ jody_rps.py
@@ -7,67 +7,86 @@
 # we will need to generate random numbers later so:
 import random
 
+# scoring variables
+
+human_victories = 0
+computer_victories = 0
+
 # list of possible actions
 actions = ["Rock", "Paper", "Scissors"]
 
-
+# doing a while loop allows us to play more than one game
 while True:
     # prompt user to enter a value: R, P or S
     player_1 = input("Please select your fighter: R = Rock, P = Paper, S = Scissors: ")
-
-# need something here for if they enter something that isn't R P or S
 
 # convert this input into Rock, Paper or Scissors
 
     def convert_player_1(player_1):
         if player_1.upper() == "R":
-            player_1_action = "Rock"
+            player_1_choice = "Rock"
         elif player_1.upper() == "P":
-            player_1_action = "Paper"
+            player_1_choice = "Paper"
         elif player_1.upper() == "S":
-            player_1_action = "Scissors"
+            player_1_choice = "Scissors"
         else:
-            player_1_action = "Invalid input"
+            player_1_choice = "Invalid input"
 
-        return player_1_action
+        return player_1_choice
 
 
     player_1_action = convert_player_1(player_1)
+
+    # to cover invalid input scenario
     if player_1_action == "Invalid input":
         print("Please enter a valid input, R, P or S")
-        break
+        continue
 
     print("You chose: ", player_1_action)
 
-    # generate a random value between 0 and 2
+    # generate a random value between 0 and 2 and use this to choose from the action list
     player_2_action = actions[random.randint(0, 2)]
     print("The computer chose: ", player_2_action)
 
-    # compare the user's choice against the computer's choice and then display a message as to if the user won, lost or drew
-    def who_wins(player_1_action, player_2_action):
+    # compares user's choice against computer's choice and then display a message as to if the user won, lost or drew
+    def who_wins(player_1_action, player_2_action, computer_victories, human_victories):
         if player_1_action == player_2_action:
             print("Both players selected the same, it is a tie")
         elif player_1_action == "Rock":
             if player_2_action == "Scissors":
                 print("Rock breaks scissors, so you are victorious!")
+                human_victories += 1
+                return human_victories
             else:
                 print("Paper wraps rock, so you have been defeated")
+                computer_victories += 1
+                return computer_victories
         elif player_1_action == "Paper":
             if player_2_action == "Rock":
                 print("Paper wraps rock, so you are victorious!")
+                human_victories += 1
+                return human_victories
             else:
                 print("Scissors cut paper, so you have been defeated")
+                computer_victories += 1
+                return computer_victories
         elif player_1_action == "Scissors":
             if player_2_action == "Paper":
                 print("Scissors cut paper, so you are victorious!")
+                human_victories += 1
+                return human_victories
             else:
                 print("Rock breaks scissors, so you have been defeated")
+                computer_victories += 1
+                return computer_victories
 
+# call the who_wins function to determine a winner:
 
-    who_wins(player_1_action, player_2_action)
+    who_wins(player_1_action, player_2_action, computer_victories, human_victories)
+    print("Total human victories = ", human_victories, "Total computer victories = ", computer_victories)
+
     play_again = input("Do you want to play again? (Y/N): ")
     if play_again != "Y":
         break
 
-
-    # showcase what you have learned about conditional statements and create your own functions
+    # could add an ongoing score here as an extra
