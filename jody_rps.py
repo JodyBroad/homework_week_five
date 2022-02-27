a536d39b2148a3d42f0ef3a9b1d77c35d05a0e5b
Jody Broad <msjodybroad@gmail.com>
2022-02-27 13:13:35 +0000

    changes to jody_rps

--- jody_rps.py
+++ jody_rps.py
@@ -10,54 +10,64 @@ import random
 # list of possible actions
 actions = ["Rock", "Paper", "Scissors"]
 
-# prompt user to enter a value: R, P or S
-player_1 = input("Please select your fighter: R = Rock, P = Paper, S = Scissors: ")
-# print(player_1)
+
+while True:
+    # prompt user to enter a value: R, P or S
+    player_1 = input("Please select your fighter: R = Rock, P = Paper, S = Scissors: ")
 
 # need something here for if they enter something that isn't R P or S
 
 # convert this input into Rock, Paper or Scissors
 
-def convert_player_1(player_1):
-    if player_1 == "R":
-        player_1_action = "Rock"
-    elif player_1 == "P":
-        player_1_action = "Paper"
-    elif player_1 == "S":
-        player_1_action = "Scissors"
-    return player_1_action
-
-
-player_1_action = convert_player_1(player_1)
-
-print("You chose: ", player_1_action)
-
-# generate a random value between 0 and 2
-player_2_action = actions[random.randint(0, 2)]
-print("The computer chose: ", player_2_action)
-
-# compare the user's choice against the computer's choice and then display a message as to if the user won, lost or drew
-def who_wins(player_1_action, player_2_action):
-    if player_1_action == player_2_action:
-        print("Both players selected the same, it is a tie")
-    elif player_1_action == "Rock":
-        if player_2_action == "Scissors":
-            print("Rock breaks scissors, so you are victorious!")
+    def convert_player_1(player_1):
+        if player_1.upper() == "R":
+            player_1_action = "Rock"
+        elif player_1.upper() == "P":
+            player_1_action = "Paper"
+        elif player_1.upper() == "S":
+            player_1_action = "Scissors"
         else:
-            print("Paper wraps rock, so you have been defeated")
-    elif player_1_action == "Paper":
-        if player_2_action == "Rock":
-            print("Paper wraps rock, so you are victorious!")
-        else:
-            print("Scissors cut paper, so you have been defeated")
-    elif player_1_action == "Scissors":
-        if player_2_action == "Paper":
-            print("Scissors cut paper, so you are victorious!")
-        else:
-            print("Rock breaks scissors, so you have been defeated")
+            player_1_action = "Invalid input"
+
+        return player_1_action
 
 
-who_wins(player_1_action, player_2_action)
+    player_1_action = convert_player_1(player_1)
+    if player_1_action == "Invalid input":
+        print("Please enter a valid input, R, P or S")
+        break
+
+    print("You chose: ", player_1_action)
+
+    # generate a random value between 0 and 2
+    player_2_action = actions[random.randint(0, 2)]
+    print("The computer chose: ", player_2_action)
+
+    # compare the user's choice against the computer's choice and then display a message as to if the user won, lost or drew
+    def who_wins(player_1_action, player_2_action):
+        if player_1_action == player_2_action:
+            print("Both players selected the same, it is a tie")
+        elif player_1_action == "Rock":
+            if player_2_action == "Scissors":
+                print("Rock breaks scissors, so you are victorious!")
+            else:
+                print("Paper wraps rock, so you have been defeated")
+        elif player_1_action == "Paper":
+            if player_2_action == "Rock":
+                print("Paper wraps rock, so you are victorious!")
+            else:
+                print("Scissors cut paper, so you have been defeated")
+        elif player_1_action == "Scissors":
+            if player_2_action == "Paper":
+                print("Scissors cut paper, so you are victorious!")
+            else:
+                print("Rock breaks scissors, so you have been defeated")
 
 
-# showcase what you have learned about conditional statements and create your own functions
+    who_wins(player_1_action, player_2_action)
+    play_again = input("Do you want to play again? (Y/N): ")
+    if play_again != "Y":
+        break
+
+
+    # showcase what you have learned about conditional statements and create your own functions
